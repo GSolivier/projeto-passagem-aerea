257c9b919f65db2c393de4354f0c1254bd78375d
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2023-04-18 10:56:41 -0300

    Continuidade ao exercício proposto em sala de aul

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@
 //2- Listar Passagens
 //0- Sair
 
-static bool VerificadorSenha(string senhaC, string senhaU){
+static bool VerificadorSenha(string senhaC, string senhaU)
+{
 
     // return bool.Parse(senhaC = senhaU);
     if (senhaC == senhaU)
@@ -20,10 +21,29 @@ static bool VerificadorSenha(string senhaC, string senhaU){
     }
 }
 
+static void BarraCarregamento(string carregando, int tempo, int pontos)
+{
+
+    Console.Write($"{carregando}");
+
+    for ( var i = 0; i < pontos; i++)
+    {
+        Console.Write($".");
+        Thread.Sleep(tempo);
+    }
+}
+
+string[] nomePassageiro = new string[1000];
+string[] origem = new string[1000];
+string[] destino = new string[1000];
+string[] dataVoo = new string[1000];
+
 string password = "admin123";
 string senhaUser;
 bool loopBreak = true;
-
+int escolhaMenu;
+int escolhaCadastro;
+bool sairCadastro = false;
 
 
 Console.WriteLine(@$"
@@ -38,37 +58,35 @@ Console.WriteLine(@$"
 
 do
 {
-senhaUser = Console.ReadLine();
+    senhaUser = Console.ReadLine();
 
-if (VerificadorSenha(password, senhaUser))
+    if (VerificadorSenha(password, senhaUser))
+    {
+        Console.Clear();
+        Console.WriteLine($"Senha Correta");
+        Console.WriteLine($"Aguarde...");
+        Thread.Sleep(5000);
+        Console.Clear();
+        loopBreak = false;
+    }
+
+    else
+    {
+        Console.Clear();
+        Console.WriteLine($"Senha incorreta, tente novamente");
+        Console.WriteLine();
+        Console.WriteLine($"Digite a senha para desbloquear o acesso: ");
+        loopBreak = true;
+    }
+
+} while (loopBreak);
+
+BarraCarregamento("Carregando Informações", 800, 6 );
+
+
+do
 {
-    Console.Clear();
-    Console.WriteLine($"Senha Correta");
-    Console.WriteLine($"Aguarde...");
-    Thread.Sleep(5000);
-    Console.Clear();
-    loopBreak = false;
-}
-
-else{
-    Console.Clear();
-    Console.WriteLine($"Senha incorreta, tente novamente");
-    Console.WriteLine();
-    Console.WriteLine($"Digite a senha para desbloquear o acesso: ");
-    loopBreak = true;
-}
-    
-} while (loopBreak);        
-
-
-Console.Write($"Carregando informações");
-
-for (var i = 0; i < 6; i++)
-{
-    Console.Write($".");
-    Thread.Sleep(800);
-}
-
+    startOfLoop:
 Console.WriteLine(@$"
            MENU INICIAL
     
@@ -79,3 +97,85 @@ Console.WriteLine(@$"
         2 - Listar passagens
         0 - Fechar programa  
 ");
+    escolhaMenu = int.Parse(Console.ReadLine());
+
+    if (escolhaMenu == 1)
+    {
+        Console.Clear();
+                    Console.WriteLine(@$"
+        MENU DE CADASTRO DE PASSAGENS
+
+    Aqui você irá cadastrar suas passagens.
+    Informe todas as informações corretamente,
+    para evitar eventuais problemas.
+            ");
+    
+
+
+        for (var i = 0; i < origem.Length; i++)
+        {
+            Console.WriteLine($"Informe o nome do titular da passagem:");
+            nomePassageiro [i] = Console.ReadLine();
+            
+            Console.WriteLine($"Informe a origem da sua viagem:");
+            origem [i] = Console.ReadLine();
+            
+            Console.WriteLine($"Informe o destino da sua viagem:");
+            destino [i] = Console.ReadLine();
+            
+            Console.WriteLine($"Informe a data do seu voo:");
+            dataVoo [i] = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(@$"
+            Deseja cadastras mais alguma passagem?
+            [1] - SIM
+            [2] - Retornar para o menu
+            ");
+            escolhaCadastro = int.Parse(Console.ReadLine());
+                // Console.WriteLine($"{nomePassageiro [i]} {origem [i]} {destino [i]} {dataVoo [i]}");
+
+            if (escolhaCadastro == 2)
+            {
+                
+                goto startOfLoop;
+            }
+
+            else{
+                Console.WriteLine($"");  
+            }
+            
+        }
+    }
+
+    else if(escolhaMenu == 2)
+    {
+        Console.WriteLine($"Aqui estão todas as suas passagens cadastradas:");
+
+        for (var i = 0; i < origem.Length; i++){
+
+            if (nomePassageiro[i] == null)
+            {
+                
+                Console.WriteLine($"Sem mais passagens cadastradas........");
+                goto endOfList;
+                
+            }
+            
+            Console.WriteLine($"---------------------------------------------");
+            Console.WriteLine($"");
+            Console.WriteLine($"ID da passagem: #0{i + 1}2023");
+            Console.WriteLine($"Nome do titular: {nomePassageiro[i]}");
+            Console.WriteLine($"Origem da viagem: {origem[i]}");
+            Console.WriteLine($"Destino da viagem: {destino[i]}");
+            Console.WriteLine($"Data da viagem: {dataVoo[i]}");
+            Console.WriteLine($"");
+        }
+        endOfList:
+            Console.WriteLine($"O que deseja fazer?");
+            Console.WriteLine($"[1] - retornar ao menu");
+            Console.WriteLine($"[2] - sair");
+    }
+    
+} while (loopBreak);
